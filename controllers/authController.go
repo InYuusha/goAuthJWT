64fb119bfa5e2278ee0a020f5e341b2d3081b184
InYuusha <asuse975@gmail.com>
2021-08-08 03:00:17 +0530

    cookie

--- controllers/authController.go
+++ controllers/authController.go
@@ -3,10 +3,15 @@ package controllers
 import (
 	"auth/db"
 	"auth/models"
+	"strconv"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const secretKey = "Key"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -25,7 +30,7 @@ func Register(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 }
-func Login(c *fiber.Ctx)error{
+func Login(c *fiber.Ctx) error {
 
 	var data map[string]string
 	err := c.BodyParser(&data)
@@ -34,21 +39,40 @@ func Login(c *fiber.Ctx)error{
 		return err
 	}
 	var user models.User
-	db.DB.Where("email=?",data["email"]).First(&user)
+	db.DB.Where("email=?", data["email"]).First(&user)
 
-	if user.Id==0{
+	if user.Id == 0 {
 		c.Status(fiber.StatusNotFound)
 		return c.JSON(fiber.Map{
-			"Message":"User not found",
+			"Message": "User not found",
 		})
 	}
-	if err:=bcrypt.CompareHashAndPassword(user.Password,[]byte(data["password"]));err!=nil{
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
-			"Message":"Incorrect Password",
+			"Message": "Incorrect Password",
 		})
 	}
-	return c.JSON(user)
-
+	//token
+	clams := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.StandardClaims{
+		Issuer:strconv.Itoa(int(user.Id)),
+		ExpiresAt:time.Now().Add(time.Hour * 24).Unix(),
+	})
+	token,err:=clams.SignedString([]byte(secretKey))
+	if err!=nil{
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message":"Could not login",
+		})
+	}
+	cookie:=fiber.Cookie{
+		Name:"jwt",
+		Value:token,
+		Expires: time.Now().Add(time.Hour * 24),
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message":"Success",
+	})
 
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module auth
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gofiber/fiber/v2 v2.16.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	gorm.io/driver/mysql v1.1.1
